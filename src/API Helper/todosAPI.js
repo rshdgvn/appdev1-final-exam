5d069804a21d3b732d30fe834c68cdb7b5644ff7
rshdgvn <rasheedgavinesponga@gmail.com>
2025-11-29 08:37:35 +0800

    Implement API helper using axios

--- src/API Helper/todosAPI.js
+++ src/API Helper/todosAPI.js
@@ -1,8 +1,27 @@
-import { configureStore } from "@reduxjs/toolkit";
-import todosReducer from "../features/todos/todosSlice";
+import axios from "axios";
 
-export const store = configureStore({
-  reducer: {
-    todos: todosReducer,
-  },
-});
+const API_URL = import.meta.env.VITE_APP_API_URL;
+
+export const getTodosAPI = function () {
+  return axios.get(`${API_URL}/todos?_limit=10`).then((res) => res.data);
+};
+
+export const addTodoAPI = function (todo) {
+  return axios
+    .post(`${API_URL}/todos`, todo, {
+      headers: { "Content-Type": "application/json" },
+    })
+    .then((res) => res.data);
+};
+
+export const updateTodoAPI = function (todo) {
+  return axios
+    .put(`${API_URL}/todos/${todo.id}`, todo, {
+      headers: { "Content-Type": "application/json" },
+    })
+    .then((res) => res.data);
+};
+
+export const deleteTodoAPI = function (id) {
+  return axios.delete(`${API_URL}/todos/${id}`).then(() => id);
+};
