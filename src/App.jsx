79b80aeb337fa733890284224515ccdc7912e04a
rshdgvn <rasheedgavinesponga@gmail.com>
2025-11-29 11:10:11 +0800

    Install GitHub pages for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,19 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Pages/Login";
 import Home from "./Pages/Home";
 import Todos from "./Pages/Todos";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <Home />,
-    },
-    {
-      path: "/login",
-      element: <Login />,
-    },
-    {
-      path: "/todos",
-      element: <Todos />,
-    },
-  ],
-  {
-    basename: "/appdev1-final-exam/", 
-  }
-);
-
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <BrowserRouter basename="/appdev1-final-exam">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/todos" element={<Todos />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
